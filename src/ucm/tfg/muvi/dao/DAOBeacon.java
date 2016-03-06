de57f684bf359d7a8c1240c21db196ff5f0207ba
Jorge Rueda <jorueda@ucm.es>
2016-03-06 23:33:01 +0100

    Service Beacon added

---
+++ src/ucm/tfg/muvi/dao/DAOBeacon.java
@@ -0,0 +1,54 @@
+package ucm.tfg.muvi.dao;
+
+import java.util.ArrayList;
+
+import org.bson.Document;
+
+import com.mongodb.client.MongoDatabase;
+
+import ucm.tfg.muvi.entities.Beacon;
+import ucm.tfg.muvi.util.MongoClientUtil;
+
+public class DAOBeacon {
+	
+	public void crear(Beacon beacon) throws Exception {
+		MongoDatabase db = MongoClientUtil.getMongoDatabase();
+		ArrayList<Document> busqueda = db.getCollection("beacon").find(
+				new Document("id_usuario", beacon.getID_usuario())
+				.append("id_beacon", beacon.getID_beacon())).into(new ArrayList<Document>());
+		if (busqueda.isEmpty()) {
+			db.getCollection("beacon").insertOne(
+					new Document()
+						.append("id_usuario", beacon.getID_usuario())
+						.append("id_beacon", beacon.getID_beacon())
+					);
+		} else {
+			throw new Exception("ese usuario ya existe en el beacon");
+		}
+	}
+
+	public ArrayList<Document> listar(Long id_beacon) throws Exception {
+		
+		MongoDatabase db = MongoClientUtil.getMongoDatabase();
+		ArrayList<Document> busqueda = db.getCollection("beacon").find(
+				new Document("id_beacon", id_beacon)).into(new ArrayList<Document>());
+		if (busqueda.isEmpty()) {
+			throw new Exception("beacon sin usuarios");
+		} else {
+			return busqueda;
+		}
+		
+	}
+	public void eliminar (Beacon beacon) throws Exception {
+		
+		MongoDatabase db = MongoClientUtil.getMongoDatabase();
+		Long count= db.getCollection("beacon").deleteOne(
+				new Document("id_usuario", beacon.getID_usuario())
+				.append("id_beacon", beacon.getID_beacon())).getDeletedCount();
+		
+		if (count == 0 ) {
+			throw new Exception("ese usuario no existe en el beacon");
+		} 
+	}
+	
+}
---
+++ src/ucm/tfg/muvi/services/ServicioBeacon.java
@@ -0,0 +1,106 @@
+package ucm.tfg.muvi.services;
+
+
+
+import java.util.ArrayList;
+import java.util.Iterator;
+
+import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+import org.bson.Document;
+import org.codehaus.jettison.json.JSONException;
+import org.codehaus.jettison.json.JSONObject;
+
+import ucm.tfg.muvi.dao.DAOBeacon;
+import ucm.tfg.muvi.entities.Beacon;
+import ucm.tfg.muvi.util.ErrorToJson;
+
+@Path("/beacon")
+public class ServicioBeacon {
+	
+	
+	@POST
+	@Consumes(MediaType.APPLICATION_JSON)
+	@Produces(MediaType.APPLICATION_JSON)
+	public Response agregar(String data) throws JSONException {
+		JSONObject json = new JSONObject(data);
+		Iterator<?> val = json.keys();
+		Beacon beacon = new Beacon();
+		String key;
+		while (val.hasNext()) {
+			key = (String) val.next();
+		    if (key.equals("id_usuario")) {
+		    	beacon.setID_usuario(Long.parseLong(json.getString("id_usuario")));
+		    } else if (key.equals("id_beacon")) {
+		    	beacon.setID_beacon(Long.parseLong(json.getString("id_beacon")));
+		    }
+		}
+		
+		DAOBeacon dao = new DAOBeacon();
+		try {
+			dao.crear(beacon);
+		} catch (Exception e) {
+			return Response.status(422).entity(new ErrorToJson(e.getMessage())).build();
+		}
+		return Response.status(201).entity(beacon).build();
+	}
+	
+	
+	@GET
+	@Produces(MediaType.APPLICATION_JSON)
+	public Response listar(@QueryParam("id_beacon") String id_beacon) throws JSONException {
+		
+		Long beacon = Long.parseLong(id_beacon);
+		ArrayList<Document> lista;
+		DAOBeacon dao = new DAOBeacon();
+		try {
+			lista = dao.listar(beacon);
+		} catch (Exception e) {
+			return Response.status(422).entity(new ErrorToJson(e.getMessage())).build();
+		}
+		return Response.status(200).entity(lista).build();
+	}
+	
+	
+	
+	@DELETE
+	@Consumes(MediaType.APPLICATION_JSON)
+	@Produces(MediaType.APPLICATION_JSON)
+	public Response eliminar(String data) throws JSONException {
+		JSONObject json = new JSONObject(data);
+		Iterator<?> val = json.keys();
+		Beacon beacon = new Beacon();
+		String key;
+		while (val.hasNext()) {
+			key = (String) val.next();
+		    if (key.equals("id_usuario")) {
+		    	beacon.setID_usuario(Long.parseLong(json.getString("id_usuario")));
+		    } else if (key.equals("id_beacon")) {
+		    	beacon.setID_beacon(Long.parseLong(json.getString("id_beacon")));
+		    }
+		}
+		
+		DAOBeacon dao = new DAOBeacon();
+		try {
+			dao.eliminar(beacon);
+		} catch (Exception e) {
+			return Response.status(422).entity(new ErrorToJson(e.getMessage())).build();
+		}
+		return Response.status(200).entity(beacon).build();
+	}
+	
+	
+	
+	
+}
+
+
+
