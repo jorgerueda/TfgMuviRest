3f8efade79bd46186adca533e34b48101836b70d
Cristina Delgado <cris3wdev@gmail.com>
2016-04-15 20:20:50 +0200

    Add recommender service

---
+++ src/ucm/tfg/muvi/services/ServicioRecomendador.java
@@ -0,0 +1,43 @@
+package ucm.tfg.muvi.services;
+
+import java.io.File;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+import org.apache.mahout.cf.taste.impl.model.file.FileDataModel;
+import org.apache.mahout.cf.taste.impl.neighborhood.ThresholdUserNeighborhood;
+import org.apache.mahout.cf.taste.impl.recommender.GenericUserBasedRecommender;
+import org.apache.mahout.cf.taste.impl.similarity.PearsonCorrelationSimilarity;
+import org.apache.mahout.cf.taste.model.DataModel;
+import org.apache.mahout.cf.taste.neighborhood.UserNeighborhood;
+import org.apache.mahout.cf.taste.recommender.UserBasedRecommender;
+import org.apache.mahout.cf.taste.similarity.UserSimilarity;
+
+import ucm.tfg.muvi.util.ErrorToJson;
+
+@Path("/recomendador")
+public class ServicioRecomendador {
+	
+	@GET
+	@Produces(MediaType.APPLICATION_JSON)
+	public Response estimar(@QueryParam("usuario") String usuario, @QueryParam("pelicula") String pelicula ) {
+		long userID = Long.parseLong(usuario);
+		long filmID = Long.parseLong(pelicula);
+		float estimacion = 0;
+		try {
+			DataModel model = new FileDataModel(new File("C:/WorkspaceMaven/MuviAppREST/dataset/ratings.csv"));
+			UserSimilarity similarity = new PearsonCorrelationSimilarity(model);
+			UserNeighborhood neighborhood = new ThresholdUserNeighborhood(0.1, similarity, model);
+			UserBasedRecommender recommender = new GenericUserBasedRecommender(model, neighborhood, similarity);
+			estimacion = recommender.estimatePreference(userID, filmID);
+		} catch (Exception e) {
+			return Response.status(422).entity(new ErrorToJson("error inesperado")).build();
+		}
+		return Response.status(200).entity(estimacion).build();
+	}
+}
