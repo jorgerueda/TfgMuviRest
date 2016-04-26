8bbd484aaf741f0cffbafd997500d8247e7a4460
Jorge Rueda <jorueda@ucm.es>
2016-04-26 10:46:56 +0200

    rama de pruebas

--- src/ucm/tfg/muvi/services/ServicioRecomendador.java
+++ src/ucm/tfg/muvi/services/ServicioRecomendador.java
@@ -1,7 +1,13 @@
 package ucm.tfg.muvi.services;
 
 import java.io.File;
+import java.net.URI;
+import java.net.URL;
 
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.servlet.ServletContext;
+import javax.servlet.http.HttpServlet;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
@@ -22,8 +28,11 @@ import ucm.tfg.muvi.util.ErrorToJson;
 import ucm.tfg.muvi.util.EstimacionToJson;
 
 @Path("/recomendador")
-public class ServicioRecomendador {
-	
+public class ServicioRecomendador{
+	@javax.ws.rs.core.Context 
+	ServletContext context;
+
+
 	@GET
 	@Produces(MediaType.APPLICATION_JSON)
 	public Response estimar(@QueryParam("usuario") String usuario, @QueryParam("pelicula") String pelicula ) {
@@ -31,8 +40,22 @@ public class ServicioRecomendador {
 		long filmID = Long.parseLong(pelicula);
 		float valor = 0;
 		EstimacionToJson estimacion = new EstimacionToJson();
+		String prueba = "prueba";
 		try {
-			DataModel model = new FileDataModel(new File("C:/WorkspaceMaven/MuviAppREST/dataset/ratings.csv"));
+			
+			/*Context initCtx= new InitialContext();
+			Context envCtx = (Context) initCtx.lookup("java:comp/env/dataset");
+			//String storagePath = (String) envCtx.lookup("dataset");*/
+
+			//File file = FileUtils.toFile(new URL("File://container.fdi.ucm.es:20041/dataset/ratings.csv"));
+			//URI url = new URI("http://container.fdi.ucm.es:20041/dataset/ratings.csv");
+			//File file1 = new File (context.getRealPath("/WEB-INF/classes/ratings.csv"));
+			File file1 = new File(context.getRealPath("/WEB-INF/classes/ucm/tfg/muvi/services/ratings.csv"));
+
+			//URL resourceUrl = URL.class.getResource("/ratings.csv");
+			//File file= new File (resourceUrl.toURI());
+			DataModel model = new FileDataModel(file1);
+			
 			UserSimilarity similarity = new PearsonCorrelationSimilarity(model);
 			UserNeighborhood neighborhood = new ThresholdUserNeighborhood(0.1, similarity, model);
 			UserBasedRecommender recommender = new GenericUserBasedRecommender(model, neighborhood, similarity);
